5a7ef2c48885c04a2b36b81091c84eb875d1c41b
greenlaser <55558448+greeenlaser@users.noreply.github.com>
2025-02-18 20:55:02 +0200

    setting up project

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+//Copyright(C) 2025 Lost Empire Entertainment
+//This program comes with ABSOLUTELY NO WARRANTY.
+//This is free software, and you are welcome to redistribute it under certain conditions.
+//Read LICENSE.md for more information.
+
+#include <iostream>
+
+#include "main.hpp"
+
+using std::cout;
+
+namespace Core
+{
+	void Physics::Test1()
+	{
+		cout << "test1 was ran from ElypsoPhysics!\n";
+	}
+}
